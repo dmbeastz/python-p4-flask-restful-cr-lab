4fa3e3e0b8095130151541953630530c2e8c75db
Dave <daveroymutisya2@gmail.com>
2024-04-09 09:38:35 +0300

    Finished Lab

--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy_serializer import SerializerMixin # type: ignore
 
 db = SQLAlchemy()
 
@@ -7,3 +7,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    image = db.Column(db.String(80))
+    price = db.Column(db.Numeric(precision=10, scale=2))  # Using Numeric for decimal data type
